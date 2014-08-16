75e5fd9d541bf4526f6c57923fd639086ca69916
Pawel Grzybek <studiorgb@users.noreply.github.com>
2014-08-16 16:11:55 +0100

    Inistail commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	plumber = require('gulp-plumber'),
+	uglify = require('gulp-uglify'),
+	rename = require("gulp-rename");
+
+
+gulp.task('sass', function () {
+	gulp.src('./sass/style.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(autoprefixer('safari >= 6', 'chrome >= 31', 'ie >= 8', 'ff >= 24', 'ios >= 4', 'android >= 4'))
+		.pipe(minifycss())
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('uglify', function () {
+	gulp.src('./js/script.js')
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(rename('script.min.js'))
+		.pipe(gulp.dest('./js'));
+});
+
+
+gulp.task('watch', function() {
+	gulp.watch('sass/**/*.scss', ['sass']);
+	gulp.watch('js/**/*.js', ['uglify']);
+});
+
+
+gulp.task('default', ['watch']);
