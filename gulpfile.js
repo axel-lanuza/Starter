0274fa67423a604c01fad95fb78e7802e4e017b8
Pawel Grzybek <studiorgb@users.noreply.github.com>
2014-09-23 06:35:30 +0100

    Changed gulp-sass to gulp-ruby-sass. Removed gulp-css-minify plugin, and adding option to generate sourceMaps for sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
+	sass = require('gulp-ruby-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
-	minifycss = require('gulp-minify-css'),
 	plumber = require('gulp-plumber'),
 	uglify = require('gulp-uglify'),
 	rename = require("gulp-rename");
@@ -10,15 +9,16 @@ var gulp = require('gulp'),
 gulp.task('sass', function () {
 	gulp.src('./sass/style.scss')
 		.pipe(plumber())
-		.pipe(sass())
+		.pipe(sass({
+      style: 'compressed',
+      precision: 4
+    }))
 		.pipe(autoprefixer())
-		.pipe(minifycss())
 		.pipe(gulp.dest('./css'));
 });
 
 gulp.task('uglify', function () {
 	gulp.src('./js/script.js')
-		.pipe(plumber())
 		.pipe(uglify())
 		.pipe(rename('script.min.js'))
 		.pipe(gulp.dest('./js'));
