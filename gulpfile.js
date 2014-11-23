b7c5d7dea1ca11b94f220e9adb752c57d25072c5
Pawel Grzybek <studiorgb@users.noreply.github.com>
2014-11-23 23:12:26 +0000

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,40 @@
 var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	uglify = require('gulp-uglify'),
-	rename = require("gulp-rename"),
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  rename = require("gulp-rename"),
   imagemin = require('gulp-imagemin'),
   pagespeed = require('psi');
 
 
 // Compile sass, minify css, autoprefix
 gulp.task('sass', function () {
-	gulp.src('./sass/style.scss')
-		.pipe(sass({
+  gulp.src('./sass/style.scss')
+    .pipe(sass({
       style: 'compressed',
       precision: 4
     }))
     .on('error', function (err) {
       console.log(err.message);
     })
-		.pipe(autoprefixer())
-		.pipe(gulp.dest('./css'));
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./css'));
 });
 
 
 // Minify js files
 gulp.task('uglify', function () {
-	gulp.src('./js/scripts.js')
-		.pipe(uglify())
-		.pipe(rename('scripts.min.js'))
-		.pipe(gulp.dest('./js'));
+  gulp.src('./js/scripts.js')
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('./js'));
 });
 
 
 // Watch sass and js changes
 gulp.task('watch', function() {
-	gulp.watch('sass/**/*.scss', ['sass']);
-	gulp.watch('js/**/*.js', ['uglify']);
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('js/**/*.js', ['uglify']);
 });
 
 
