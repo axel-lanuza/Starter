82694500a3b7289711a304aa9ced68941669b73f
Pawel Grzybek <studiorgb@users.noreply.github.com>
2015-08-01 13:21:07 +0100

    Linters added

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,37 @@
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  parker = require('gulp-parker');
-
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var parker = require('gulp-parker');
+var jscs = require('gulp-jscs');
+var scsslint = require('gulp-scss-lint');
 
 // Compile sass to compressed css andd add vendor prefixes
-gulp.task('styles', function () {
+gulp.task('styles', function() {
   gulp.src('./sass/style.scss')
     .pipe(sass({
-      outputStyle: 'compressed',
+      outputStyle: 'compressed'
     }))
-    .on('error', function (error) {
+    .on('error', function(error) {
       console.log('- - - ERROR - - - \n' + error.message);
     })
     .pipe(autoprefixer({
-      browsers: ['> 1%', 'last 2 versions', 'Firefox >= 20'],
+      browsers: ['> 1%', 'last 2 versions', 'Firefox >= 20']
     }))
     .pipe(gulp.dest('./css'));
 });
 
-
 // Concatenate files and minify to output to scripts.min.js
-gulp.task('scripts', function () {
+gulp.task('scripts', function() {
   gulp.src(['./js/script1.js', './js/script2.js'])
     .pipe(concat('scripts.min.js'))
     .pipe(uglify())
-    .on('error', function (error) {
+    .on('error', function(error) {
       console.log('- - - ERROR - - - \n' + error.message);
     })
     .pipe(gulp.dest('./js'));
 });
-
 
 // Watch sass and js changes
 gulp.task('watch', function() {
@@ -40,13 +39,23 @@ gulp.task('watch', function() {
   gulp.watch('js/**/*.js', ['scripts']);
 });
 
-
 // CSS analysis tool
 gulp.task('parker', function() {
   return gulp.src('./css/style.css')
     .pipe(parker());
 });
 
+// Lint .js files
+gulp.task('js-lint', function() {
+  return gulp.src(['js/script1.js', 'js/script2.js', './gulpfile.js'])
+    .pipe(jscs());
+});
+
+// Lint .scss files
+gulp.task('scss-lint', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(scsslint());
+});
 
 // default task
 gulp.task('default', ['watch']);
